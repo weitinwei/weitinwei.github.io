eafeacf18145ead0eee130c701938e0b5b4f3223
weitinwei <37700925+weitinwei@users.noreply.github.com>
2018-04-14 20:35:56 +0800

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -22,9 +22,9 @@
 	require_once "Smtp.class.php";
 	//******************** 配置信息 ********************************
 	$smtpserver = "smtp.163.com";//SMTP服务器
-	$smtpserverport =25;//SMTP服务器端口
+	$smtpserverport =465;//SMTP服务器端口
 	$smtpusermail = "15778045813@163.com";//SMTP服务器的用户邮箱
-	$smtpemailto = $_GAT['123'];//发送给谁
+	$smtpemailto = $_GAT['15778045813@163.com'];//发送给谁
 	$smtpuser = "15778045813@163.com";//SMTP服务器的用户帐号，注：部分邮箱只需@前面的用户名
 	$smtppass = "qq232123456789";//SMTP服务器的用户密码
 	$mailtitle = $_GAT['title'];//邮件主题
@@ -32,7 +32,7 @@
 	$mailtype = "HTML";//邮件格式（HTML/TXT）,TXT为文本邮件
 	//************************ 配置信息 ****************************
 	$smtp = new Smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证.
-	$smtp->debug = false;//是否显示发送的调试信息
+	$smtp->debug = true;//是否显示发送的调试信息
 	$state = $smtp->sendmail($smtpemailto, $smtpusermail, $mailtitle, $mailcontent, $mailtype);
 
 	echo "<div style='width:300px; margin:36px auto;'>";
